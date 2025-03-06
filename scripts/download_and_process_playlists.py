ea07ce16d68c704f93aaf60e0620bbffcd116b5f
MXC1 <mfhcor@gmail.com>
2025-03-06 19:36:45 +0000

    More line breaks

--- scripts/download_and_process_playlists.py
+++ scripts/download_and_process_playlists.py
@@ -37,23 +37,23 @@ try:
                 print(f"Downloading Spotify playlist: {comment}")
                 subprocess.run(["sldl", url], check=True)
         elif isinstance(item, str) and "soundcloud.com" in item:
-            print(f"Processing SoundCloud playlist: {item}")
+            print(f"\nProcessing SoundCloud playlist: {item}")
             
-            print("Parsing SoundCloud playlist and printing to CSV...")
+            print("\nParsing SoundCloud playlist and printing to CSV...")
             csv_path = convert_soundcloud_to_csv(item)
             
-            print(f"Passing SoundCloud CSV to sldl: {csv_path}")
+            print(f"\nPassing SoundCloud CSV to sldl: {csv_path}")
             subprocess.run(["sldl", "--desperate", "--strict-artist", csv_path], check=True)
             
-            print(f"Removing CSV")
+            print(f"\nRemoving CSV")
             os.remove(csv_path)
 
     # Rename m3u8 playlists
-    print("Renaming playlists...")
+    print("\nRenaming playlists...")
     rename_playlists("../tracks_and_playlists/")
 
     # Remux all files to mp3 320kbps
-    print("Remuxing files to mp3 320kbps...")
+    print("\nRemuxing files to mp3 320kbps...")
     remux_to_mp3_320("../tracks_and_playlists/")
 
     print("All tasks completed!")
@@ -61,9 +61,9 @@ try:
 except subprocess.CalledProcessError as e:
     error_message = f"Command '{e.cmd}' returned non-zero exit status {e.returncode}.\n{e.stderr}"
     log_error_to_file("download_and_process_playlists.py", error_message)
-    print(f"An error occurred. Details written to the log file: {e}")
+    print(f"\nAn error occurred. Details written to the log file: {e}")
 
 except Exception as e:
     error_message = str(e)
     log_error_to_file("download_and_process_playlists.py", error_message)
-    print(f"An unexpected error occurred. Details written to the log file: {e}")
+    print(f"\nAn unexpected error occurred. Details written to the log file: {e}")
